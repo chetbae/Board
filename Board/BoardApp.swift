e699cb0f21400477782ec36932704efa45bf3dec
chetbae <48145854+chetbae@users.noreply.github.com>
2024-10-26 18:08:25 -0400

    Initial Commit

---
+++ Board/BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardApp.swift
+//  Board
+//
+//  Created by max on 2024-10-26.
+//
+
+import SwiftUI
+
+@main
+struct BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
